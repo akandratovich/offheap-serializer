eaf19b711466072cbdf4fd9daaa6366acccb4cf0
Andrew Kondratovich <andrew.kondratovich@gmail.com>
2012-03-10 02:18:54 +0300

    ignore

--- src/test/java/org/machariel/test/Tries.java
+++ src/test/java/org/machariel/test/Tries.java
@@ -2,12 +2,17 @@ package org.machariel.test;
 
 import static org.junit.Assert.*;
 
-import java.util.Random;
+import java.util.*;
 
 import org.junit.Test;
-import org.machariel.core.util.U;
+import org.machariel.core.util.*;
+import org.machariel.test.util.*;
 
 import sun.misc.Unsafe;
+
+class A {
+  int a = 7;
+}
 
 public class Tries {
   private static final Random r = new Random();
@@ -15,27 +20,49 @@ public class Tries {
   
   @Test
   public void method() {
-    Object[] a = new Object[1];
-    a[0] = new Integer(1);
-    
-    System.out.println(a.getClass().getComponentType());
-    
-    time(new Runnable() {
-      @Override
-      public void run() {
-        
-      }
-    });
+    A a = new A();
+
+    long ref = U.o2a(a);
+    // long ref = U.o2a(new ArrayList().getClass());
+    // byte[] hex = new byte[20];
+    // for (int i = 0; i < 20; i++) hex[i] = u.getByte(ref + i);
+
+    // System.out.println(Common.hex(hex));
+    // System.out.println();
+
+    long ka = u.getAddress(ref + 4);
+    // long assc = u.getAddress(ref + 20);
+
+    // hex = new byte[80];
+    // for (int i = 0; i < 80; i++) hex[i] = u.getByte(ka + i);
+    // System.out.println(Common.hex(hex));
+    // System.out.println();
+
+    // int offset = u.getInt(ka + 16);
+    // hex = new byte[80];
+    // for (int i = 0; i < 80; i++) hex[i] = u.getByte(ka + offset + 16 + i);
+    // System.out.println(Common.hex(hex));
+    // System.out.println();
+    // System.out.println(offset);
+
+    // long ca = u.getAddress(ka + 36 + 16 + 12);
+    // Object cc = U.a2o(ca);
+    // System.out.println(cc);
   }
-  
-  private static void time(Runnable r) {
-    time(r, "time");
-  }
-  
-  private static void time(Runnable r, String message) {
-    long a = System.currentTimeMillis();
-    r.run();
-    long b = System.currentTimeMillis();
-    System.out.println(message + ": " + (b - a));
+
+  @Test
+  public void check() {
+    long time = 0;
+    // while (true) {
+    for (int i = 0; i < 10; i++) {
+      byte[] q = new byte[1024 * 1024 * 128];
+      long ref = U.o2a(new ArrayList());
+      long ka = u.getAddress(ref + 4); // klass pointer
+      // if (System.currentTimeMillis() - time > 2000) {
+        System.out.println(ka);
+        // time = System.currentTimeMillis();
+      // }
+      System.gc();
+    }
   }
 }
